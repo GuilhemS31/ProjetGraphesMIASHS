ea90af525f2f645dd8b51e5f841322240c0193bb
DarkVasDehors <sibade.arnaud@gmail.com>
2017-12-12 18:03:26 +0100

    caca

--- code/src/Graphes.java
+++ code/src/Graphes.java
@@ -1,4 +1,7 @@
-
 public class Graphes {
 	
+	//JSON de test
+	String maTasse = "{couleur: Rouge}";
+	JSONObject tasse = new JSONObject(maTasse);
+	System.out.println(tasse.getString(couleur)); //Rouge
 }
