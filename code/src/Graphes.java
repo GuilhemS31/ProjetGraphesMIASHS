d634d384bcb78f5558003d02448ab1878fd3465c
GuilhemS31 <guilhem31.susa@hotmail.com>
2017-12-19 10:07:13 +0100

    add constructeur Station

--- code/src/Graphes.java
+++ code/src/Graphes.java
@@ -8,25 +8,21 @@ import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 public class Graphes {
 
+	public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
 
-	
-	    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+		JSONParser parser = new JSONParser();
+		JSONArray a = (JSONArray) parser.parse(new FileReader("../data/ToulouseLight.json"));
+		
+		ArrayList<Station> mesStations = new ArrayList<Station>();
 
-	        JSONParser parser = new JSONParser();
-	        JSONArray a = (JSONArray) parser.parse(new FileReader("../data/ToulouseLight.json"));
-	        ArrayList<Station> mesStations = new ArrayList<Station>();
-	        
-	        
-	        for (int i= 0; i<a.size();i++)
-	        {
+		for (int i = 0; i < a.size(); i++) {
+			JSONObject o = (JSONObject) a.get(i);
+			mesStations.add(new Station((String) o.get("number"), (String) o.get("name"), (String) o.get("adress"),
+					(double) o.get("latitude"), (double) o.get("longitude")));
+		}
+	}
 
-	        	JSONObject o=(JSONObject)a.get(i);
-	        	mesStations.add(new Station(o.get("number"),o.get("name"),o.get("adress"),o.get("latitude"),o.get("longitude")));
-	        	//JSONObject person = (JSONObject) o;
-	          
-	        }
-	    }
-	
 }
