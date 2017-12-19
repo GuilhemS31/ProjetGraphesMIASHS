5efb7679f1e7fe3b3c1b60baf9735d80424873cd
DarkVasDehors <sibade.arnaud@gmail.com>
2017-12-19 09:13:10 +0100

    Du JSON ?

--- code/src/Graphes.java
+++ code/src/Graphes.java
@@ -1,9 +1,30 @@
-import org.json.JSONObject;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Iterator;
 
+import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 public class Graphes {
+
+
 	
-	//JSON de test
-	String maTasse = "{couleur: Rouge}";
-	JSONObject tasse = new JSONObject(maTasse);
-	System.out.println(tasse.getString(couleur)); //Rouge
+	    public static void main(String[] args) throws FileNotFoundException, IOException, ParseException {
+
+	        JSONParser parser = new JSONParser();
+
+	        JSONArray a = (JSONArray) parser.parse(new FileReader("../data/Toulouse.json"));
+	        for (Object o : a)
+	        {
+	        	//System.out.println(o);
+	        	System.out.println(JSONValue.toJSONString(o));
+	          //JSONObject person = (JSONObject) o;
+
+	          
+	        }
+	    }
+	
 }
